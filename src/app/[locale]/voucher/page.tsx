8abc73ee534d0d5f05c055005ab8be7a93519a92
mykolakotokhinhb <144212165+mykolakotokhinhb@users.noreply.github.com>
2023-11-07 04:25:01 +0200

    fixed react-i18next provider usage

--- src/app/[locale]/voucher/page.tsx
+++ src/app/[locale]/voucher/page.tsx
@@ -1,9 +1,18 @@
 import { VoucherCard } from '~/app/[locale]/voucher/_components/voucher-card';
+import TranslationsProvider from '~/components/translations-provider';
 
-export default function VoucherPage() {
+const i18nNamespaces = ['voucher'];
+
+export default function VoucherPage({
+    params: { locale },
+}: {
+    params: { locale: string };
+}) {
     return (
-        <main className="flex items-center justify-center flex-1">
-            <VoucherCard />
-        </main>
+        <TranslationsProvider namespaces={i18nNamespaces} locale={locale}>
+            <main className="flex items-center justify-center flex-1">
+                <VoucherCard />
+            </main>
+        </TranslationsProvider>
     );
 }
