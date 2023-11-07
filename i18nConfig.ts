e5b57d68ec8ecb54b351e9e392b765290fb315c5
mykolakotokhinhb <144212165+mykolakotokhinhb@users.noreply.github.com>
2023-11-07 02:41:24 +0200

    removed unused key form i18n config

--- i18nConfig.ts
+++ i18nConfig.ts
@@ -3,9 +3,6 @@ import { type Config } from 'next-i18n-router/dist/types';
 const i18nConfig: Config = {
     locales: ['en', 'ua'],
     defaultLocale: 'en',
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    routingStrategy: 'dynamicSegment',
 };
 
 export default i18nConfig;
